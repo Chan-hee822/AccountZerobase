15d7c0e85417ebe2b05e3315ca907f31309b68e5
H-chanhee <83047523+H-chanhee@users.noreply.github.com>
2023-04-24 04:51:40 +0900

    Initial commit

---
+++ src/main/java/com/example/accountzerobase/AccountZerobaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountzerobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountZerobaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountZerobaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountzerobase/AccountZerobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountzerobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountZerobaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
