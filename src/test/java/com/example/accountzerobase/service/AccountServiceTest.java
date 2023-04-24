695fc1559b9a811aa2ce3b29e1146aabf5fbd477
H-chanhee <83047523+H-chanhee@users.noreply.github.com>
2023-04-24 18:07:35 +0900

    mocking, controller test, verify

--- src/test/java/com/example/accountzerobase/service/AccountServiceTest.java
+++ src/test/java/com/example/accountzerobase/service/AccountServiceTest.java
@@ -2,22 +2,67 @@ package com.example.accountzerobase.service;
 
 import com.example.accountzerobase.domain.Account;
 import com.example.accountzerobase.domain.AccountStatus;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.accountzerobase.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
-    @Autowired
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
     private AccountService accountService;
 
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testxxx() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789")
+                        .build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+
+        //when
+        Account account = accountService.getAccount(4555L);
+
+        //then
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(4555L, captor.getValue());
+        assertNotEquals(45551L, captor.getValue());
+        assertEquals("65789",account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED,account.getAccountStatus());
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로조회")
+    void testFailedToSearchAccount() {
+        //given
+        //when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+
+        //then
+
+        assertEquals("Minus",exception.getMessage());
     }
 
     @Test
